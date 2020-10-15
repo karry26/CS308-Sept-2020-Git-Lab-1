06df6cee195fddb6d693828f25db872e79ebee17
Kairav <kairavbansal@gmail.com>
2020-10-16 00:21:36 +0530

    checked the input number

--- sqrt.c
+++ sqrt.c
@@ -4,7 +4,20 @@ positive number. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+bool isnum(char *num)
+{
+    int i=0;
+    while(num[i]!='\0')
+    {
+        if(i==0)
+        {
+            if(num[i]!='-' && (num[i]<'0' || num[i]>'9'))return false;
+        }
+        else if((num[i]<'0' || num[i]>'9')) return false;
+        i++;
+    }
+    return true;
+}
 //program to get sqrt of number
 int main(int argc, char* argv[]) {
 
@@ -12,6 +25,11 @@ int main(int argc, char* argv[]) {
 		printf("Usage: sqrt input\n\n");
 		exit(-1);
 	}
+	if(!isnum(argv[1]))
+    {
+        printf("please enter a valid number\n");
+        return 0;
+    }
 
 	int input = atoi(argv[1]);
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
