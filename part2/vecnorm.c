ee3f366e38f1fc36e6d4bd39403c283f8699fc88
Hridayesh <hridayes.akhand@gmail.com>
2020-10-17 02:25:51 +0530

    modified vecnorm.c

--- part2/vecnorm.c
+++ part2/vecnorm.c
@@ -4,7 +4,7 @@
 float norm(vector vec_a,vector vec_b)
 {
     float norm1 = 0.0;
-    for (int i=0;i<DIM;i++) {
+    for (int i = 0; i < dim; i++) {
         norm1 = norm1 + (vec_a[i] - vec_b[i]) * (vec_a[i] - vec_b[i]);
     }
     return sqrt(norm1);
